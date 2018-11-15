6501ed8315deb5bc9f06411b7fb41cce9b96d3c6
Linus Jäderlund <linus.jaderlund@headlight.se>
2018-11-15 19:24:29 +0100

    Adds listing pizzas

--- views/list/list.js
+++ views/list/list.js
@@ -1,3 +1,33 @@
 (function(app) {
+  const renderList = (products) => {
+    let html = '';
+    const el = document.getElementsByClassName('js-list-table')[0];
 
+    for (const product of products) {
+      html += `
+        <tr class="list-table__row">
+          <td class="list-table__column">${product.product}</td>
+          <td class="list-table__column">${product.ingredients}</td>
+          <td class="list-table__column list-table__column--right">&dollar;${product.price.USD}</td>
+          <td class="list-table__column list-table__column--right">
+            <button class="[ js-add-cart ]">Add to cart</button>
+          </td>
+        </tr>
+      `;
+    }
+
+    el.innerHTML = html;''
+  }
+
+  const init = () => {
+    app.xhr('GET', 'api/product', null, (status, products) => {
+      console.log(status, products);
+
+      if (status === 200 && products) {
+        renderList(products);
+      }
+    });
+  };
+
+  init();
 }(window.app = window.app || {}));
